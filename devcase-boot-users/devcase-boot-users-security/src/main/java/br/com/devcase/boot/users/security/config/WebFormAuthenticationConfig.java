652c4dfe0b9bab4137ec4ba13400895bc16dd9ed
Thiago Hirata <hirata@devcase.com.br>
2017-10-09 16:19:34 -0300

    Login - mudança para thymeleaf

--- devcase-boot-users/devcase-boot-users-security/src/main/java/br/com/devcase/boot/users/security/config/WebFormAuthenticationConfig.java
+++ devcase-boot-users/devcase-boot-users-security/src/main/java/br/com/devcase/boot/users/security/config/WebFormAuthenticationConfig.java
@@ -1,18 +1,10 @@
 package br.com.devcase.boot.users.security.config;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.nio.charset.Charset;
-import java.util.Map;
-
-import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.apache.commons.text.StringEscapeUtils;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
@@ -20,7 +12,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.core.annotation.Order;
-import org.springframework.core.io.Resource;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -29,36 +20,34 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.WebAttributes;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.View;
-
-import com.google.common.io.ByteStreams;
-import com.google.common.primitives.Bytes;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
 @Configuration
 @ConditionalOnWebApplication
 @EnableWebSecurity()
-@Import({CommonSecurityConfig.class})
-public class WebFormAuthenticationConfig  {
+@Import({ CommonSecurityConfig.class })
+public class WebFormAuthenticationConfig {
 	public static final int WEBFORM_SECURITY_ORDER = SecurityProperties.BASIC_AUTH_ORDER + 1;
-	
+
 	@Order(WEBFORM_SECURITY_ORDER)
 	@Configuration
 	static class WebFormSecurityConfigurer extends WebSecurityConfigurerAdapter {
-		
+
 		public WebFormSecurityConfigurer() {
 			super(false);
 		}
 
 		@Override
 		protected void configure(HttpSecurity http) throws Exception {
-			http
-				.authorizeRequests()
-					.anyRequest().authenticated()
-					.and()
-				.formLogin()
-					.loginPage("/login")
-					.permitAll();
+			http.authorizeRequests()
+				.anyRequest().authenticated()
+				.and()
+					.formLogin().loginPage("/login").permitAll().and()
+					.logout().logoutUrl("/logout").logoutSuccessUrl("/login");
 		}
 
 		@Override
@@ -66,90 +55,35 @@ public class WebFormAuthenticationConfig  {
 			super.init(web);
 			web.ignoring().antMatchers("/css/**", "/js/**", "/images/**", "/**/favicon.ico");
 		}
-		
-		
+
 	}
-	
+
 	@Controller
-	@ConditionalOnClass(value=HttpServletRequest.class)
+	@ConditionalOnClass(value = HttpServletRequest.class)
 	@Lazy
 	static class FormLoginController {
-		
-		@Value("${devcase-boot.users.security.login-form-file:classpath:/defaultLogin.html}")
-		Resource loginFormResource;
-		@Value("${devcase-boot.users.security.login-form-file.charset:ISO-8859-1}")
-		String charset;
-		
-		byte[] pageContents;
-		int csrfNamePos;
-		int csrfNameSize;
-		int csrfTokenPos;
-		int csrfTokenSize;
-		int errorMessagePos;
-		int errorMessageSize;
-		
-		@PostConstruct
-		void parseLoginForm() throws IOException {
-			try (InputStream is = loginFormResource.getInputStream()) {
-				pageContents = ByteStreams.toByteArray(is);
-				Charset charset = Charset.forName(this.charset);
 
-				csrfNamePos = Bytes.indexOf(pageContents, "@csrfname@".getBytes(charset));
-				csrfNameSize = "@csrfname@".getBytes(charset).length;
+		@Autowired
+		private ThymeleafViewResolver viewResolver;
+		@Autowired
+		private LocaleResolver localeResolver;
 
-				csrfTokenPos = Bytes.indexOf(pageContents, "@csrftoken@".getBytes(charset));
-				csrfTokenSize = "@csrftoken@".getBytes(charset).length;
-
-				errorMessagePos = Bytes.indexOf(pageContents, "@errormessage@".getBytes(charset));
-				errorMessageSize = "@errormessage@".getBytes(charset).length;
-
-			}
-		}
-		
-		View loginForm = new View() {
-			@Override
-			public String getContentType() {
-				return "text/html";
-			}
-			
-			@Override
-			public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
-				CsrfToken token = (CsrfToken) request.getAttribute(CsrfToken.class.getName());
-				Charset charset2 = Charset.forName(charset);
-
-				response.setContentType(getContentType());
-				try (OutputStream os = response.getOutputStream()) {
-					os.write(pageContents, 0, csrfNamePos);
-					//name
-					if (token != null) {
-						os.write(token.getParameterName().getBytes(charset2));
-					}
-					os.write(pageContents, csrfNamePos + csrfNameSize, csrfTokenPos - csrfNamePos - csrfNameSize);
-					if (token != null) {
-						os.write(token.getToken().getBytes(charset2));
-					}
-					os.write(pageContents, csrfTokenPos + csrfTokenSize, errorMessagePos - csrfTokenPos - csrfTokenSize);
-
-					HttpSession session = request.getSession(false);
-					if (session != null) {
-						AuthenticationException ex = (AuthenticationException) session.getAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
-						String errorMsg = ex != null ? ex.getMessage() : "";
-						errorMsg = StringEscapeUtils.escapeEcmaScript(errorMsg);
-						os.write(errorMsg.getBytes(charset2));
-					}
-					
-					os.write(pageContents, errorMessagePos + errorMessageSize, pageContents.length - errorMessagePos - errorMessageSize);
-
-				}
-			}
-		};
-		
 		@RequestMapping("/login")
-		View loginForm() {
-			return loginForm;
+		View loginForm(HttpServletRequest request, Model model) throws Exception {
+			CsrfToken token = (CsrfToken) request.getAttribute(CsrfToken.class.getName());
+			model.addAttribute("csrfToken", token);
+
+			HttpSession session = request.getSession(false);
+			if (session != null) {
+				AuthenticationException ex = (AuthenticationException) session
+						.getAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
+				String errorMsg = ex != null ? ex.getMessage() : "";
+				errorMsg = StringEscapeUtils.escapeEcmaScript(errorMsg);
+				model.addAttribute("authenticationErrorMessage", errorMsg);
+			}
+			return viewResolver.resolveViewName("login", localeResolver.resolveLocale(request));
 		}
-		
+
 	}
 
-	
 }
