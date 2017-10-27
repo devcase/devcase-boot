da33f02dd49d3ec11f501b3125c4a1ac79bae755
Thiago Hirata <hirata@devcase.com.br>
2017-10-27 00:48:43 -0200

    Acertos em autoconfiguração do webcrud

--- devcase-boot-libs/devcase-boot-webcrud/src/main/java/br/com/devcase/boot/webcrud/autoconfigure/WebCrudAutoConfiguration.java
+++ devcase-boot-libs/devcase-boot-webcrud/src/main/java/br/com/devcase/boot/webcrud/autoconfigure/WebCrudAutoConfiguration.java
@@ -8,22 +8,24 @@ import javax.persistence.metamodel.EntityType;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.data.rest.RepositoryRestMvcAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
-import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import br.com.devcase.boot.webcrud.criteria.CriteriaSourceArgumentResolver;
 
 @Configuration
-@PropertySource(value="classpath:/br/com/devcase/boot/webcrud/properties/web-crud.properties")
-@AutoConfigureAfter(value=RepositoryRestMvcAutoConfiguration.class)
+@PropertySource(value = "classpath:/br/com/devcase/boot/webcrud/properties/web-crud.properties")
 public class WebCrudAutoConfiguration {
 
 	/**
@@ -33,14 +35,15 @@ public class WebCrudAutoConfiguration {
 	 *
 	 */
 	@Configuration
-	static class CriteriaRepositorySupportConfiguration {
+	@AutoConfigureBefore(WebMvcAutoConfiguration.class)
+	public static class CriteriaRepositorySupportConfiguration {
 		@Bean
 		public CriteriaSourceArgumentResolver criteriaSourceArgumentResolver() {
 			return new CriteriaSourceArgumentResolver();
 		}
 
 		@Configuration
-		static class WebMvcCriteriaConfigurer implements WebMvcConfigurer {
+		public static class WebMvcCriteriaConfigurer implements WebMvcConfigurer {
 
 			@Autowired
 			private CriteriaSourceArgumentResolver criteriaSourceArgumentResolver;
@@ -51,26 +54,24 @@ public class WebCrudAutoConfiguration {
 			}
 		}
 	}
-	
-	@Configuration
-	@ConditionalOnBean(value=RepositoryRestMvcConfiguration.class)
-	public static class ExposeIds extends RepositoryRestConfigurerAdapter {
-		@Autowired
-		private EntityManagerFactory emf;
 
-		@Override
-		public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
-			Set<EntityType<?>> entities = emf.getMetamodel().getEntities();
-			config.exposeIdsFor(entities.stream().map(e -> e.getJavaType()).toArray(size -> new Class<?>[size]));
+	@Configuration
+	static class ExposeIdsRestMvcConfiguration {
+
+		@Configuration
+		public static class ExposeIdsConfigurer extends RepositoryRestConfigurerAdapter {
+			@Autowired(required=false)
+			private EntityManagerFactory emf;
+
+			@Override
+			public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+				if(emf == null) return;
+				Set<EntityType<?>> entities = emf.getMetamodel().getEntities();
+				config.exposeIdsFor(entities.stream().map(e -> e.getJavaType()).toArray(size -> new Class<?>[size]));
+			}
+
 		}
+
 	}
 
-//	@Configuration
-//	static class JsonFormatCustomizerConfiguration {
-//		@Bean
-//		public Jackson2ObjectMapperBuilder objectMapperBuilder() {
-//			Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
-//			return builder;
-//		}
-//	}
 }
--- devcase-boot-users/devcase-boot-users-webadmin/src/test/java/br/com/devcase/boot/users/webadmin/ExposeIdsTest.java
+++ devcase-boot-users/devcase-boot-users-webadmin/src/test/java/br/com/devcase/boot/users/webadmin/ExposeIdsTest.java
@@ -1,30 +1,40 @@
 package br.com.devcase.boot.users.webadmin;
 
-import org.junit.Assert;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import br.com.devcase.boot.webcrud.autoconfigure.WebCrudAutoConfiguration.ExposeIds;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
-@ContextConfiguration(classes = UsersWebAdminApplication.class)
+@ContextConfiguration(classes = { UsersWebAdminApplication.class, TestDataSetup.class } )
 @EnableAutoConfiguration
 @ActiveProfiles({"test", "test-h2"})
 public class ExposeIdsTest {
-	
-	@Autowired(required=false)
-	ExposeIds exposeIds;
-	
+	@Autowired
+	WebApplicationContext wac;
+
 	@Test
-	public void testExposeIdsPresent() {
-		Assert.assertNotNull(exposeIds);
+	public void testExposeIdsPresent() throws Exception {
+		MockMvc mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+		mockMvc.perform(get("/api/users").with(user("root").roles("ADMIN_USERS")))
+			 .andDo(print())
+			 .andDo(log())
+			 .andExpect(status().isOk())
+			 .andExpect(jsonPath("$._embedded.users[*].id").exists());
 	}
 }
