e0677e6878a31e6122beef99a1cf50241151538d
Thiago Hirata <hirata@devcase.com.br>
2017-11-06 12:06:29 -0200

    configuração para teste automático

--- devcase-boot-users/devcase-boot-users-domain/src/test/java/br/com/devcase/boot/users/domain/RestRepositoryConfig.java
+++ devcase-boot-users/devcase-boot-users-domain/src/test/java/br/com/devcase/boot/users/domain/RestRepositoryConfig.java
@@ -3,6 +3,7 @@ package br.com.devcase.boot.users.domain;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import br.com.devcase.boot.users.domain.autoconfigure.DevcaseUsersAutoConfiguration;
@@ -10,7 +11,7 @@ import br.com.devcase.boot.users.domain.autoconfigure.DevcaseUsersAutoConfigurat
 @Configuration
 @EnableAutoConfiguration
 @EnableWebMvc
-@Import(DevcaseUsersAutoConfiguration.class)
+@Import({DevcaseUsersAutoConfiguration.class, RepositoryRestMvcConfiguration.class})
 public class RestRepositoryConfig {
 
 }
