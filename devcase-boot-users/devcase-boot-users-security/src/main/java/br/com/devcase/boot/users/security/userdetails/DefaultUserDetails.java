f9655997634e62ff445dbe6fdfc3db0c2534aa95
Bruno Klava <bruno@tvod.com.br>
2019-03-26 15:37:23 -0300

    Detalhes login

--- devcase-boot-users/devcase-boot-users-security/src/main/java/br/com/devcase/boot/users/security/userdetails/DefaultUserDetails.java
+++ devcase-boot-users/devcase-boot-users-security/src/main/java/br/com/devcase/boot/users/security/userdetails/DefaultUserDetails.java
@@ -13,32 +13,37 @@ import br.com.devcase.boot.users.domain.entities.PasswordCredential;
 import br.com.devcase.boot.users.domain.entities.Permission;
 
 public class DefaultUserDetails extends User {
-	
-	/**
-	 * 
-	 */
+
 	private static final long serialVersionUID = 6732550151456123873L;
-	private final String userId;
+
+	private String name;
+	private String email;
 
 	public DefaultUserDetails(br.com.devcase.boot.users.domain.entities.User user,
 			PasswordCredential passwordCredential, List<? extends Permission> permissions) {
 		super(user.getUsername(), passwordCredential != null ? passwordCredential.getPassword() : "", user.isEnabled(),
 				!user.isExpired(), passwordCredential != null ? !passwordCredential.isExpired() : true,
 				!user.isLocked(), convertToAuthorities(permissions));
-		this.userId = user.getId();
-		if(passwordCredential == null) {
+
+		name = user.getName();
+		email = user.getUsername();
+
+		if (passwordCredential == null) {
 			this.eraseCredentials();
 		}
 	}
 
-	private static Collection<? extends GrantedAuthority> convertToAuthorities(List<? extends Permission> roles) {
-		return roles.stream().map(s -> new SimpleGrantedAuthority("ROLE_".concat(s.getRole())))
-				.distinct().collect(Collectors.toList());
+	public String getName() {
+		return name;
 	}
 
-//	@Override
-//	public String getUserId() {
-//		return userId;
-//	}
+	public String getEmail() {
+		return email;
+	}
+
+	private static Collection<? extends GrantedAuthority> convertToAuthorities(List<? extends Permission> roles) {
+		return roles.stream().map(s -> new SimpleGrantedAuthority("ROLE_".concat(s.getRole()))).distinct()
+				.collect(Collectors.toList());
+	}
 
 }
